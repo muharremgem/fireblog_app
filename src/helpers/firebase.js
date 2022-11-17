a15b5e73122ae8af0eec308ce9075c55f9c339ee
muharrem gem <muharremgem28@gmail.com>
2022-11-17 19:44:45 +0300

    firebase

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGxKtN-2_NRYW_jBvmuZN9_Q_XlXZnLAw",
-  authDomain: "fireblogapp-52cb0.firebaseapp.com",
-  projectId: "fireblogapp-52cb0",
-  storageBucket: "fireblogapp-52cb0.appspot.com",
-  messagingSenderId: "346312743483",
-  appId: "1:346312743483:web:685341160e56c8a3599a1e",
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_ID,
+  appId: REACT_APP_API_ID,
 };
 
 // Initialize Firebase
