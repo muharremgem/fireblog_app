7a85d254ee606f1a803d5604cc7d4e0e323988fc
muharrem gem <muharremgem28@gmail.com>
2022-11-17 15:39:14 +0300

    Components solutıon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import AppRouter from "./app-router/AppRouter";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-  
+    <div>
+      <AppRouter />
     </div>
   );
 }
