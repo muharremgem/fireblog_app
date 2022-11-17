4103e9b77e73b22fb2c29eb45bc00fca42ff61b1
muharrem gem <muharremgem28@gmail.com>
2022-11-17 16:50:44 +0300

    register

--- src/app-router/AppRouter.js
+++ src/app-router/AppRouter.js
@@ -3,12 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import Navbar from "../components/Navbar";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Layout from "../components/Layout";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Layout>
+      <div>muharrem</div>
+    </Layout>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
