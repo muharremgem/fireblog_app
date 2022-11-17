1683d7c2fa16a87f8cbe7154fb9a1106e9d280be
muharrem gem <muharremgem28@gmail.com>
2022-11-17 15:52:55 +0300

    navbar

--- src/app-router/AppRouter.js
+++ src/app-router/AppRouter.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Navbar from "../components/Navbar";
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
