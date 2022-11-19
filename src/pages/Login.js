420c9b366a007bf373e65a97c3b570d2858f652d
muharrem gem <muharremgem28@gmail.com>
2022-11-19 23:28:24 +0300

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,7 +22,7 @@ function Copyright(props) {
       {...props}
     >
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" href="https://muharremgem.com">
         Your Website
       </Link>{" "}
       {new Date().getFullYear()}
