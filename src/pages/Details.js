619c99d87f621f95b2b43216b4566c38edbf9bb0
muharrem gem <muharremgem28@gmail.com>
2022-11-20 18:17:07 +0300

    details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Details = () => {
   return (
-    <div>Details</div>
-  )
-}
+    <div>
+      <div>details</div>
+    </div>
+  );
+};
 
-export default Details
+export default Details;
