257b62246e1261414ab9a33f442db500a882db4e
muharrem gem <muharremgem28@gmail.com>
2022-11-20 18:15:17 +0300

    register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -61,6 +61,7 @@ export default function SignUp() {
                   autoFocus
                 />
               </Grid>
+
               <Grid item xs={12} sm={6}>
                 <TextField
                   required
