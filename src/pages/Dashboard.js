622ee8952b3d22adc933461f473a761064c288ce
muharrem gem <muharremgem28@gmail.com>
2022-11-21 18:22:18 +0300

    card

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Layout from "../components/Layout";
+import BlogCard from "../components/BlogCard";
 
 const Dashboard = () => {
   return (
     <Layout>
-      <div>ikkan</div>
+      <div className="">
+        <div className="flex justify-center items-center  text-5xl mt-5">
+          <p className="">--------- Dashboard ---------</p>
+        </div>
+        <BlogCard className="flex mt-5" />
+      </div>
     </Layout>
   );
 };
