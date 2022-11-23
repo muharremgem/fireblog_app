34920d8499bfbf64da96ecf94650079fa43e4733
muharrem gem <muharremgem28@gmail.com>
2022-11-23 13:56:32 +0300

    blogContext

--- src/contexts/BlogContext.js
+++ src/contexts/BlogContext.js
@@ -1,89 +1,19 @@
-import { onValue, push, ref, remove, set, update } from "firebase/database";
-import React, { createContext, useState, useEffect } from "react";
-import { auth, db } from "../helpers/firebase";
+import { createContext, useState } from "react";
 
 export const BlogContext = createContext();
 
 export const BlogProvider = ({ children }) => {
-  // Form State
   const [title, setTitle] = useState("");
   const [image, setImage] = useState("");
   const [textArea, setTextArea] = useState("");
-  const [isUpdate, setIsUpdate] = useState(false);
-  // BlogData
-  const [blogData, setBlogData] = useState([]);
 
-  const handleSubmitForm = (state) => {
-    if (state !== undefined) {
-      updateBlog(state);
-    } else {
-      const newPost = {
-        user_id: auth.currentUser.uid,
-        username: auth.currentUser.displayName,
-        title,
-        image,
-        textArea,
-        like: [],
-        comment: [],
-      };
-      saveToDatabasePost(newPost);
-    }
-  };
 
-  const saveToDatabasePost = (newPost) => {
-    const postRef = ref(db, "Blog");
-    const newPostRef = push(postRef);
-    set(newPostRef, {
-      ...newPost,
-    });
-  };
+  
 
-  const updateBlog = (state) => {
-    const verifyUser = auth.currentUser.uid === state.user_id;
-    if (verifyUser) {
-      update(ref(db, "Blog/" + state.id), {
-        ...state,
-        title,
-        image,
-        textArea,
-      });
-      setTextArea("");
-      setTitle("");
-      setImage("");
-    }
-  };
-
-  const deleteFromDatabase = (oldPost) => {
-    remove(ref(db, "Blog/" + oldPost.id));
-  };
-
-  useEffect(() => {
-    const postRef = ref(db, "Blog");
-    onValue(postRef, (items) => {
-      const data = items.val();
-      const DataArr = [];
-      for (let id in data) {
-        DataArr.push({ id, ...data[id] });
-      }
-      setBlogData(DataArr);
-    });
-  }, []);
 
   return (
     <BlogContext.Provider
-      value={{
-        setTitle,
-        title,
-        setImage,
-        image,
-        setTextArea,
-        textArea,
-        handleSubmitForm,
-        blogData,
-        deleteFromDatabase,
-        setIsUpdate,
-        isUpdate,
-      }}
+      value={(title, setTitle, image, setImage, textArea, setTextArea)}
     >
       {children}
     </BlogContext.Provider>
--- src/pages/NewBlog.js
+++ src/pages/NewBlog.js
@@ -3,11 +3,16 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Layout from "../components/Layout";
 import { Button } from "@mui/material";
+import { BlogContext } from "../contexts/BlogContext";
+import { useContext } from "react";
 
 export default function BasicTextFields() {
+  const { setTitle, setImage, setTextArea } = useContext(BlogContext);
+
   return (
     <Layout>
       <Box
+        //onSubmit={handleSubmitForm}
         component="form"
         sx={{
           "& > :not(style)": { m: 1, width: "35ch" },
@@ -22,16 +27,31 @@ export default function BasicTextFields() {
           className="rounded-lg mt-5"
         />
         <p className="text-3xl text-center">✰ New Blog ✰</p>{" "}
-        <TextField id="outlined-basic" label="Title*" variant="outlined" />
-        <TextField id="outlined-basic" label="Image URL*" variant="outlined" />
+        <TextField
+          id="outlined-basic"
+          label="Title*"
+          variant="outlined"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Image URL*"
+          variant="outlined"
+          onChange={(e) => setImage(e.target.value)}
+        />
         <TextField
           id="outlined-multiline-static"
           label="Content*"
           multiline
           rows={15}
           defaultValue=""
+          onChange={(e) => setTextArea(e.target.value)}
         />
-        <Button variant="contained" sx={{ backgroundColor: "Black" }}>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "Black" }}
+          type="submit"
+        >
           Submit
         </Button>
       </Box>
